a0e95b601d199b0669b8cc23856bc54291494415
leejunwoo0202 <gs04043@naver.com>
2021-09-09 16:05:41 +0900

    다각형 그리기

--- 2-15/2-15.cpp
+++ 2-15/2-15.cpp
@@ -46,15 +46,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
-	POINT point[10] = { { 10, 150} , {250, 30}, {500,150},
-		{350,300}, {150,300} };
+	POINT point[10] = { { 10, 150} , {250, 30}, {500,150},  // 꼭짓점의 좌표를 저장할 POINT 타입 배열 변수를 선언한다. 
+		{350,300}, {150,300} };								// 여기서는 좌표를 최대 10개 저장할 수 있는 point 배열을 지역변수로 선언하고 초깃값을 5개만 지정했다.
 
 	switch (iMsg)
 	{
 	case WM_CREATE:
 		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
+	case WM_PAINT:						// WM_PAINT 메시지가 발생했을 때 화면을 얻어오고 hdc에 꼭짓점이 5개인 다각형을 그린다. 다각형은 면이 있으므로
+		hdc = BeginPaint(hwnd, &ps);	// 내부가 색으로 채워지는데 따로 지정하지 않으면 기본 색인 흰색으로 채워진다. 반면에 선의 기본 색은 검정이다.
 		Polygon(hdc, point, 5);
 		EndPaint(hwnd, &ps);
 		break;
